ac8294421605ad399bdb98b344d7c265811a53e8
Bastian Ilso <contact@bastianilso.com>
2019-05-01 13:30:55 +0200

    Initial Project Commit

---
+++ Assets/_Scripts/Core/SceneManage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManage : MonoBehaviour
+{
+    public Transform viveCtrlLeft;
+    public Transform viveCtrlRight;
+
+    public static Transform getViveCtrlRight;
+    public static string currentScene = "Menu 0";
+    public static int loadTestScene = -2;
+    private int bCount = 0;
+    private int menuCount = 0;
+
+    void Start()
+    {
+        viveCtrlLeft.gameObject.SetActive(false);
+        viveCtrlRight.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        getViveCtrlRight = viveCtrlRight;
+    }
+
+    public void SetBaking()
+    {
+        bCount++;
+        currentScene = "Bageplade " + bCount;
+        loadTestScene = 2;
+    }
+
+    public void SetMenu()
+    {
+        menuCount++;
+        currentScene = "Menu " + menuCount;
+        loadTestScene = 0;
+    }
+}
---
+++ Assets/_Scripts/SceneTimer.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SceneTimer : MonoBehaviour
+{
+    public GameObject bakingObjs, startUI, menuObj, startBtns;
+    public Text sceneTimerTxt, totalTimerTxt, timerBtnTxt;
+    public static int sceneTimer;
+    public Image timerBtnImage;
+
+    private float sceneTimerF, totalTimerF;
+
+    public static bool timerActivated = false;
+
+    // used for data logging in LoggerBehavior.cs
+    public class SceneTimerClass
+    {
+        public static int sceneStat
+        {
+            get
+            {
+                return sceneTimer;
+            }
+        }
+    }
+
+    void Start()
+    {
+        sceneTimerTxt.text = "Scene: 0";
+        totalTimerTxt.text = "Total: 0";
+    }
+
+    void Update()
+    {
+        totalTimerF += Time.deltaTime;
+        totalTimerTxt.text = "Total: " + Mathf.RoundToInt(totalTimerF).ToString();
+
+        if (timerActivated && (SceneManage.loadTestScene == 1 || SceneManage.loadTestScene == 2))
+        {
+            sceneTimerF += Time.deltaTime;
+            sceneTimer = Mathf.RoundToInt(sceneTimerF);
+        }
+
+        if (SceneManage.loadTestScene == 2)
+        {
+            sceneTimerTxt.text = "Scene: " + Mathf.RoundToInt(sceneTimerF).ToString() + " / 300";
+
+            if (sceneTimer >= 300)
+            {
+                bakingObjs.SetActive(false);
+                ResetTimer();
+            }
+        }
+    }
+
+    public void StartTimer()
+    {
+        timerActivated = true;
+        timerBtnTxt.text = "Continue";
+    }
+
+    public void PauseTimer()
+    {
+        timerActivated = false;
+    }
+
+    public void ResetTimer()
+    {
+        sceneTimerTxt.text = "Scene: 0";
+        startUI.SetActive(true);
+        sceneTimerF = 0f;
+        timerActivated = false;
+        timerBtnTxt.text = "Start";
+    }
+
+    public void ResetToMenu()
+    {
+        ResetTimer();
+        menuObj.SetActive(true);
+        startBtns.SetActive(true);
+    }
+
+}
